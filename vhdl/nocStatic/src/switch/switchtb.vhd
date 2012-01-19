718b6e286e68cbad0b733a072753ae24e802863f
Richard Huber <rihuber@ee.ethz.ch>
2012-01-19 16:36:17 +0100

    Folder reorganization: move vhdl projects into /vhdl

---
+++ vhdl/nocStatic/src/switch/switchtb.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity switchTb is
+	-- Testbench has no input and output signals!
+end switchTb;
+
+architecture testbench of switchTb is
+begin
+	
+	
+	
+end architecture testbench;

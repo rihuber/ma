e731b82ed0d086bcc336826d4cac0ee279c6141b
Richard Huber <rihuber@ee.ethz.ch>
2012-01-25 14:13:20 +0100

     Added Java model for switch TB

---
+++ java/SwitchModel/src/ch/rihuber/switchModel/SwitchModel.java
@@ -0,0 +1,91 @@
+package ch.rihuber.switchModel;
+
+import java.io.*;
+
+public class SwitchModel 
+{
+	
+	private BufferedReader in;
+	private PrintWriter out;
+	private boolean hasNextStimulus = true;
+	private int cycles = 0;
+	
+	public SwitchModel(String stimuliFileName, String responseFileName) throws FileNotFoundException 
+	{
+		createStimuliWriter(stimuliFileName);
+		createResponseReader(responseFileName);
+	}
+	
+	public static void main(String[] args) throws Exception 
+	{
+		String stimuliFileName = null, responseFileName = null;
+		for(int i=0; i<args.length; i++)
+		{
+			if(args[i].startsWith("stimuliFileName="))
+				stimuliFileName = args[i].substring(16);
+			if(args[i].startsWith("responseFileName="))
+				responseFileName = args[i].substring(17);
+		}
+		if(stimuliFileName == null)
+			throw new Exception("No input file specified");
+		if(responseFileName == null)
+			throw new Exception("No response file specified");
+		
+		new SwitchModel(stimuliFileName, responseFileName).run();
+	}
+	
+	private void run() throws Exception 
+	{
+		try 
+		{
+			while(hasNextStimulus)
+			{
+				applyNextStimulus();
+				fetchResponse();
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			closeFiles();
+		}
+	}
+	
+	private void applyNextStimulus() throws FileNotFoundException 
+	{
+		String outLine = "0 1";
+		out.write(outLine+"\n");
+		out.flush();
+		System.out.println("Written stimulus: "+outLine);
+		
+		if(cycles++ > 5)
+			hasNextStimulus = false;
+	}
+	
+	private void fetchResponse() throws IOException 
+	{
+		String inLine = in.readLine();
+		if(inLine == null)
+			hasNextStimulus = false;
+		System.out.println("Fetched response: "+inLine);
+	}
+	
+	private void createStimuliWriter(String stimuliFileName) throws FileNotFoundException 
+	{
+		out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(stimuliFileName)));
+		System.out.println("Opened stimuli file");
+	}
+	
+	private void createResponseReader(String responseFileName) throws FileNotFoundException 
+	{
+		in = new BufferedReader(new InputStreamReader(new FileInputStream(responseFileName)));
+		System.out.println("Opened response file");
+	}
+	
+	private void closeFiles() throws IOException 
+	{
+		if(out!=null)
+			out.close();
+		if(in!=null)
+			in.close();
+	}
+}
